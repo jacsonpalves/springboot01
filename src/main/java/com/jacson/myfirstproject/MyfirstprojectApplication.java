42a5cca409e81924c255e31cce208310f2b45383
jacsonpalves <jacsonp.alves@gmail.com>
2020-11-22 22:29:38 -0300

    Category memory

---
+++ src/main/java/com/jacson/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,32 @@
+package com.jacson.myfirstproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jacson.myfirstproject.entities.Category;
+import com.jacson.myfirstproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class MyfirstprojectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+		
+	}
+
+}
